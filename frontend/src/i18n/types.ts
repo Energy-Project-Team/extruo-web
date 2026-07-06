41624339420362e9a79340a34a5085f46903601f
Stormy_RPG <Stormy_RPG@mail.ru>
2026-07-06 18:01:04 +0400

    chore: add copyright

--- frontend/src/i18n/types.ts
+++ frontend/src/i18n/types.ts
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (C) 2026 Energy Project Team
 
 /** Root translation object containing all application strings. */
 export interface Translation {
